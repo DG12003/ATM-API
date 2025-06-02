2f8b72c40fd32172356efb3d273988c4fad7948b
DG12003 <dg12003@ues.edu.sv>
2025-06-01 19:10:50 -0600

    Agregando Documentacion Swagger

--- atm/src/main/java/com/sersaprosa/atm/AtmApplication.java
+++ atm/src/main/java/com/sersaprosa/atm/AtmApplication.java
@@ -1,11 +1,17 @@
 package com.sersaprosa.atm;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "ATM API Operations", version = "1.0.0"),
+		servers = {@Server(url = "http://localhost:8080")})
 public class AtmApplication {
 
 	public static void main(String[] args) {
